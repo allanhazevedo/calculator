20226adc25ea859246ffeff871157290afb0b736
Allan Azevedo <allanhazevedo@gmail.com>
2021-03-17 20:59:36 -0300

    fixing add operator
    just do it. there´s no other way

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // warning: DO NOT remove plus signs before variables 'a' and 'b' to avoid string concatenation
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
